7cef65b1923d47a44b9ee88e4a693fc3e4999ae8
vianney diris <vianney.diris@gmail.com>
2018-12-04 19:24:22 +0100

    ajout des méthodes add,update et delete

--- src/main/java/com/lpdm/msproduct/controller/ProductController.java
+++ src/main/java/com/lpdm/msproduct/controller/ProductController.java
@@ -2,6 +2,7 @@ package com.lpdm.msproduct.controller;
 
 import com.lpdm.msproduct.dao.ProductDao;
 import com.lpdm.msproduct.entity.Product;
+import com.lpdm.msproduct.entity.Stock;
 import com.lpdm.msproduct.proxy.StockProxy;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -35,4 +36,32 @@ public class ProductController {
 
         return product;
     }
+
+    @PostMapping(value = "/products")
+    public void addProduct(@RequestBody Product product){
+
+        Stock stockAdded= stockProxy.addStock(product.getStock());
+        product.setStockId(stockAdded.getId());
+
+        Product productAdded = productDao.save(product);
+
+        if (productAdded.equals(null)){
+            System.out.println("Erreur lors de l'ajout");
+        }
+    }
+
+    @DeleteMapping(value="/products/{id}")
+    public void deleteStock(@PathVariable int id){
+        Product productDelete = productDao.findById(id);
+        productDao.deleteById(id);
+        stockProxy.deleteStock(productDelete.getStockId());
+    }
+
+    @PutMapping(value="/products")
+    public void updateStock(@RequestBody Product product){
+        product.setStockId(product.getStock().getId());
+        productDao.save(product);
+        stockProxy.updateStock(product.getStock());
+    }
+
 }
--- src/main/java/com/lpdm/msproduct/proxy/StockProxy.java
+++ src/main/java/com/lpdm/msproduct/proxy/StockProxy.java
@@ -3,8 +3,9 @@ package com.lpdm.msproduct.proxy;
 import com.lpdm.msproduct.entity.Stock;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.stereotype.Component;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @Component
 @FeignClient(name="ms-stock",url="localhost:28086")
@@ -12,4 +13,17 @@ public interface StockProxy {
 
     @RequestMapping(value = "/stocks/{id}")
     Stock findById(@PathVariable(value = "id") int id);
+
+    @RequestMapping(value = "/stocks")
+    List<Stock> listStock();
+
+    @RequestMapping(value = "/stocks")
+    Stock addStock(@RequestBody Stock stock);
+
+    @RequestMapping(value="/stocks/{id}")
+    void deleteStock(@PathVariable(value = "id") int id);
+
+    @RequestMapping(value="/stocks")
+    Stock updateStock(@RequestBody Stock stock);
+
 }
