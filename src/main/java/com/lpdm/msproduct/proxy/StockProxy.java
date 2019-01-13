0fca9ee17754b5cce6f818b8a7538400cef25007
vianney diris <vianney.diris@gmail.com>
2019-01-13 10:36:01 +0100

    ajout de zuul pour le proxy

--- src/main/java/com/lpdm/msproduct/proxy/StockProxy.java
+++ src/main/java/com/lpdm/msproduct/proxy/StockProxy.java
@@ -1,6 +1,7 @@
 package com.lpdm.msproduct.proxy;
 
 import com.lpdm.msproduct.entity.Stock;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.*;
@@ -8,24 +9,25 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @Component
-@FeignClient(name="ms-stock",url="localhost:28086")
+@FeignClient(name = "zuul-server", url = "https://zuul.lpdm.kybox.fr")
+@RibbonClient(name = "ms-stock")
 public interface StockProxy {
 
-    @RequestMapping(value = "/stocks/{id}")
+    @RequestMapping(value = "/ms-stock/stocks/{id}")
     Stock findById(@PathVariable(value = "id") int id);
 
-    @RequestMapping(value = "/stocks")
+    @RequestMapping(value = "/ms-stock/stocks")
     List<Stock> listStock();
 
-    @RequestMapping(value = "/stocks")
+    @RequestMapping(value = "/ms-stock/stocks")
     Stock addStock(@RequestBody Stock stock);
 
-    @RequestMapping(value="/stocks/{id}")
+    @RequestMapping(value="/ms-stock/stocks/{id}")
     void deleteStock(@PathVariable(value = "id") int id);
 
-    @RequestMapping(value="/stocks")
+    @RequestMapping(value="/ms-stock/stocks")
     Stock updateStock(@RequestBody Stock stock);
 
-    @RequestMapping(value = "/stocks/productor/{id}")
+    @RequestMapping(value = "/ms-stock/stocks/productor/{id}")
     List<Stock> listStockByProductor(@PathVariable(value = "id") int id);
 }
